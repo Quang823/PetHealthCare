5cdb3ccb83bfb9242939d2c784f77941b0fc6e74
Quang823 <quangdmse173565@fpt.edu.vn>
2024-06-13 23:50:40 +0700

    Update

---
+++ src/Components/Admin/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+
+// AdminDashboard.js
+import React from 'react';
+
+const AdminDashboard = () => {
+    return (
+        <div className='AdminPage'>
+            <h2>Admin Dashboard</h2>
+            <p>Welcome to the Admin Dashboard!</p>
+            {/* Các phần nội dung khác của trang admin */}
+        </div>
+    );
+}
+
+export default AdminDashboard;
