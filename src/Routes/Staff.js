70949bc73d631784169c3ac4993d069116dc57f3
Quang823 <quangdmse173565@fpt.edu.vn>
2024-06-20 23:07:55 +0700

    update staff

--- src/Routes/Staff.js
+++ src/Routes/Staff.js
@@ -3,16 +3,17 @@ import { Routes, Route, Link } from 'react-router-dom'
 import StaffLayout from '../Components/Staff/StaffLayout';
 import BookingStaff from '../Components/Staff/Booking/BookingStaff';
 import Cage from '../Components/Staff/Cage/Cage';
-const Staff = () =>{
-return(
-    <>
-    <Routes>
-        <Route path='/staff' element={<StaffLayout/>} />
-        <Route path='/bookingstaff' element={<BookingStaff/>} />
-        <Route path='/cagestaff' element={<Cage/>} />
+const Staff = () => {
+    return (
+        <>
+            <Routes>
+                <Route path='/staff' element={<StaffLayout />} />
+                <Route path='/homeStaff' element={<HomeStaff />} />
+                <Route path='/bookingstaff' element={<BookingStaff />} />
+                <Route path='/cagestaff' element={<Cage />} />
 
-    </Routes>
-</>
-)
+            </Routes>
+        </>
+    )
 }
 export default Staff
